a14124471887d265b0f40de2151b74111fcb1871
WALID Walid Nader <Walid.Nader@it-fusion.org>
2025-07-16 21:37:49 +0200

    handle age value from birthdate of the worker

---
+++ Models/AspNetUser.Partial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Marofh
+{
+    public partial class AspNetUser
+    {
+        public int? CalculatedAge
+        {
+            get
+            {
+                if (!this.DOB.HasValue)
+                    return null;
+
+                // تقويم هجري
+                HijriCalendar hijri = new HijriCalendar();
+
+                // السنة الهجرية الحالية
+                int currentHijriYear = hijri.GetYear(DateTime.Today);
+
+                // سنة الميلاد الهجرية
+                int birthHijriYear = hijri.GetYear(this.DOB.Value);
+
+                // فرق السنوات فقط
+                return currentHijriYear - birthHijriYear;
+            }
+        }
+    }
+}
